51c40ecef00b9fbe300dee09f1e11b618433bf2b
Wesdell <wesdell17@gmail.com>
2022-10-09 21:05:56 -0500

    adding improvements

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const d = document,
   $result = d.getElementById("result-op"),
   $numbers = d.querySelectorAll("input[data-math]"),
   $reset = d.querySelector("input[data-reset]"),
-  $equal = d.querySelector("input[data-equal]");
+  $equal = d.querySelector("input[data-equal]"),
+  $remove = d.querySelector("input[data-remove]");
 let interval;
 $result.textContent = "|";
 
@@ -12,20 +13,16 @@ $numbers.forEach((num) => {
     interval = setTimeout(() => {
       num.classList.remove("btn-click");
     }, 100);
-    if (
-      $result.textContent === "Invalid operation" ||
-      $result.textContent === "|"
-    ) {
-      $result.textContent = num.value;
-    } else {
-      $result.textContent += num.value;
-    }
+
+    $result.textContent === "Invalid operation" || $result.textContent === "|"
+      ? ($result.textContent = num.value)
+      : ($result.textContent += num.value);
   });
 });
 
 d.addEventListener("click", (e) => {
   if (e.target === $equal) {
-    /^([0-9]+|-?[0-9]+)(\.?[0-9]*)[-+*/]?[0-9]+(\.?[0-9]*)$/.test(
+    /^([0-9]+|-?[0-9]+)(\.?[0-9]*)([-+*/]?[0-9]+(\.?[0-9]*))+$/.test(
       $result.textContent
     )
       ? ($result.textContent = `${eval($result.textContent)}`)
@@ -33,10 +30,23 @@ d.addEventListener("click", (e) => {
 
     if ($result.textContent === "Infinity" || $result.textContent === "NaN")
       $result.textContent = "Invalid operation";
+
     clearTimeout(interval);
   }
   if (e.target === $reset) {
     $result.textContent = "|";
     clearTimeout(interval);
   }
+
+  if (e.target === $remove) removeText();
 });
+
+const removeText = () => {
+  if ($result.textContent === "|" || $result.textContent === "")
+    $result.textContent = "|";
+  if ($result.textContent !== "|")
+    $result.textContent = $result.textContent.substring(
+      0,
+      $result.textContent.length - 1
+    );
+};
